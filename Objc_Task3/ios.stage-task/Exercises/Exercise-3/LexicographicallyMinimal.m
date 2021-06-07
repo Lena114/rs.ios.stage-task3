8eaa71b85082e81ce1b47c1b59cf6bc3889074b1
Lena114 <70774158+Lena114@users.noreply.github.com>
2021-06-08 00:14:39 +0300

    feat: implement solution

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -1,15 +1,31 @@
-#import "LexicographicallyMinimal.h"
+#import "PlayersSeparator.h"
 
-@interface LexicographicallyMinimal()
+@implementation PlayersSeparator
 
-@property (nonatomic, strong) NSMutableString *resultString;
-
-@end
-
-@implementation LexicographicallyMinimal
-
--(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+- (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
+    
+    if (ratingArray.count < 3) {
+        return  0;
+    }
+    int count = 0;
+    
+    NSMutableArray *newArray = [[NSMutableArray alloc] initWithArray:ratingArray];
+    NSLog(@"new array %@", newArray);
+    
+    for (int i = 0; (i < (newArray.count - 2)); i++) {
+        for (int j = i + 1; (j < (newArray.count - 1)); j++) {
+            for (int k = j + 1; (k <newArray.count); k++) {
+                    NSNumber *player1 = [newArray objectAtIndex:i];
+                    NSNumber *player2 = [newArray objectAtIndex:j];
+                    NSNumber *player3 = [newArray objectAtIndex:k];
+                    NSLog(@"%@, %@, %@", player1, player2, player3);
+                    if (((player1.intValue > player2.intValue) && (player2.intValue > player3.intValue)) || ((player1.intValue < player2.intValue) && (player2.intValue < player3.intValue))) {
+                        count += 1;
+                    }
+            }
+        }
+    }
+    return count;
 }
 
 @end
